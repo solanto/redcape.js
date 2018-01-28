b6c72bf84cacd0fab1c49676788c70b56c6727dc
solanto <thumbtackearthlink@gmail.com>
2018-01-28 15:27:31 -0800

    Added a link to /learn, fixed the spelling of my name

--- _site/js/welcome.js
+++ _site/js/welcome.js
@@ -3,9 +3,9 @@ window.addEventListener("scroll", function(e) {
   var body = document.body;
   if (document.documentElement.scrollTop > 0) {
     arrow.className = arrow.className.replace("downarrow", "arrowfade");
-    body.className = body.className.replace("bright", "muted");
+    // body.className = body.className.replace("bright", "muted");
   } else {
     arrow.className = arrow.className.replace("arrowfade", "downarrow");
-    body.className = body.className.replace("muted", "bright");
+    // body.className = body.className.replace("muted", "bright");
   }
 });
