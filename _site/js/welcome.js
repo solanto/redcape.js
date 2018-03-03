d268665b6c9da821514b5cbb4a329769bf82576b
solanto <thumbtackearthlink@gmail.com>
2018-03-02 18:44:41 -0800

    finalized welcome page

--- _site/js/welcome.js
+++ _site/js/welcome.js
@@ -9,3 +9,33 @@ window.addEventListener("scroll", function(e) {
     // body.className = body.className.replace("muted", "bright");
   }
 });
+
+var moveRotation = -1; // start by going left
+var moveSpan = document.getElementById("move");
+moveSpan.className = "rotateleft";
+
+function rotateMove() {
+  if (moveRotation == 1) {
+    moveSpan.className = moveSpan.className.replace("rotateleft", "rotateright");
+    moveRotation = -1;
+  } else {
+    moveSpan.className = moveSpan.className.replace("rotateright", "rotateleft");
+    moveRotation = 1;
+  }
+}
+
+setInterval(rotateMove, 250);
+
+var colorSpan = document.getElementById("color");
+function colorBeat() {
+  colorSpan.className = "mint";
+  setTimeout(function() {colorSpan.className = "";}, 20);
+}
+
+setInterval(colorBeat, 1200);
+
+var inputSpan = document.getElementById("input");
+function stretchInput() {
+  inputSpan.className = "stretch";
+  setTimeout(function() {inputSpan.className = "";}, 500);
+}
--- index.md
+++ index.md
@@ -12,12 +12,12 @@ redcape.clothes.color = rgb(255, 0, 0);
 
 ---
 
-Responsive pages are the future of the web. Many of the webpages you use every day are much more than static; they <span id="move">move</span>, change <span id="color">color</span>, and react to your <span id="input">input</span>.
+Responsive pages are the future of the web. Many of the webpages you use every day are much more than static; they <span><span id="move">move</span></span>, change <span id="color">color</span>, and react to your <span id="input" onclick="stretchInput();">input</span>.
 
 The instructions Mother gives to Red-Cape are spoken in English.
 > Stay on the path and don't run, or you might fall.
 
-The instructions that tell a browser how a page should act are written in JavaScript.
+The instructions that tell a browser how a page should act can be written in JavaScript.
 ```javascript
 redcape.stayOnPath = true;
 redcape.walk();
